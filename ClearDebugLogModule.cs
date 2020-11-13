1148d45cff0d124d42942b4523a04aa10fdb36a3
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2020-11-13 16:46:08 +0300

    Uploaded all files

---
+++ ClearDebugLogModule.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClearDebugLog
+{
+    public class ClearDebugLogModule : ETGModule
+    {
+        public override void Init()
+        {
+        }
+
+        public override void Start()
+        {
+            ETGModConsole.Commands.AddUnit("clear_debug", this.ClearDebugLog);
+        }
+
+        public void ClearDebugLog(string[] args)
+        {
+            ETGModDebugLogClearer.ClearDebugLog();
+        }
+
+        public override void Exit()
+        {
+        }
+    }
+
+    public class ETGModDebugLogClearer : ETGModDebugLogMenu
+    {
+        public static void ClearDebugLog()
+        {
+            List<LoggedText> listOfText = _AllLoggedText;
+            if(Instance != null)
+            {
+                Instance.GUI.Children.Clear();
+            }
+            listOfText.Clear();
+            _LoggedTextAddIndex = 0;
+        }
+    }
+}
